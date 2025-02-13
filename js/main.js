bdedd009eeedc5f5e2da3612fcaa470997c21d3e
karlssonhannah <kaha24qk@student.ju.se>
2025-02-13 10:18:53 +0100

    adding event listener

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ const textBoxElement = document.getElementById("pop-up-text-table-book");
 const coffeeBookImage = document.getElementById("coffee-book-img");
 
 coffeeBookImage.addEventListener("mouseover", function (event) {
-  textBoxElement.style.fontSize = "20px";
   textBoxElement.style.fontWeight = "bold";
+});
+
+coffeeBookImage.addEventListener("mouseleave", function (event) {
+  textBoxElement.style.fontWeight = "normal";
 });
