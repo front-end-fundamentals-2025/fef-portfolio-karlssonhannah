384af49923820b690964a47ff2c3bae175fdeb6d
karlssonhannah <kaha24qk@student.ju.se>
2025-02-17 11:24:50 +0100

    adding hovering

--- js/main.js
+++ js/main.js
@@ -3,8 +3,12 @@ const coffeeBookImage = document.getElementById("coffee-book-img");
 
 coffeeBookImage.addEventListener("mouseover", function (event) {
   textBoxElement.style.fontWeight = "bold";
+  textHoverElement.style.display = "block";
 });
 
 coffeeBookImage.addEventListener("mouseleave", function (event) {
   textBoxElement.style.fontWeight = "normal";
+  textHoverElement.style.display = "none";
 });
+
+const textHoverElement = document.getElementById("hover-text");
